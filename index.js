f9fc33eca0658199ab617d9a5b27f081e80459bf
bazarghan <bazarghan80@gmail.com>
2023-02-05 16:45:50 +0330

    StepResponse Ploted

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ let slider1 = document.getElementById("myRange1");
 let slider2 = document.getElementById("myRange2");
 let slider3 = document.getElementById("myRange3");
 const ctx = canvas.getContext("2d");
-const squareNumberHorizontally = 40;
-const squareNumberVertically = 26;
-const squareSize = 24;
+const squareNumberHorizontally = 30;
+const squareNumberVertically = 20;
+const squareSize = 36;
 const width = squareSize * squareNumberHorizontally;
 const height = squareSize * squareNumberVertically;
+const xc = width/5;
+const yc = 4*(height/5);
+
 canvas.width = width;
 canvas.height = height;
 slider1.style.width = width.toString() + "px";
@@ -19,33 +22,58 @@ function drawGrid() {
   ctx.fillRect(0, 0, width, height);
   //draw two center line
   ctx.beginPath();
-  ctx.moveTo(width / 2, height);
-  ctx.lineTo(width / 2, 0);
+  ctx.moveTo(xc, height);
+  ctx.lineTo(xc, 0);
   ctx.lineWidth = 2;
   ctx.strokeStyle = "#2e3749";
   ctx.stroke();
 
   ctx.beginPath();
-  ctx.moveTo(width, height / 2);
-  ctx.lineTo(0, height / 2);
+  ctx.moveTo(width,yc);
+  ctx.lineTo(0, yc);
   ctx.lineWidth = 2;
   ctx.strokeStyle = "#2e3749";
   ctx.stroke();
   //draw Grid
   ctx.globalAlpha = 0.3;
   ctx.lineWidth = 0.3;
-  for (let x = 0; x < width; x += squareSize) {
-    for (let y = 0; y < height; y += squareSize) {
-      ctx.strokeRect(x, y, squareSize, squareSize);
-      ctx.strokeStyle = "#2e3749";
-      ctx.stroke();
-    }
+  for(let x=xc; x < width; x += squareSize){
+      for(let y=yc; y<height; y += squareSize){
+          ctx.strokeRect(x, y, squareSize, squareSize);
+          ctx.strokeStyle = "#2e3749";
+          ctx.stroke();
+      }
   }
+  for(let x=xc; x>=0; x -= squareSize){
+      for(let y=yc; y>=0; y -= squareSize){
+          ctx.strokeRect(x-squareSize, y-squareSize, squareSize, squareSize);
+          ctx.strokeStyle = "#2e3749";
+          ctx.stroke();
+      }
+  }
+  
+  for(let x=xc; x < width; x += squareSize){
+      for(let y=yc;  y>=0 ; y -= squareSize){
+          ctx.strokeRect(x, y - squareSize, squareSize, squareSize);
+          ctx.strokeStyle = "#2e3749";
+          ctx.stroke();
+      }
+  }
+  for(let x=xc; x >= 0; x -= squareSize){
+      for(let y=yc; y< height; y += squareSize){
+          ctx.strokeRect(x-squareSize, y, squareSize, squareSize);
+          ctx.strokeStyle = "#2e3749";
+          ctx.stroke();
+      }
+  }
+
+
+  
 }
 
 function mapCordinate(x, y) {
-  let newx = Math.floor(x * squareSize) + width / 2;
-  let newy = -Math.floor(y * squareSize) + height / 2;
+  let newx = Math.floor(x * squareSize) + xc;
+  let newy = -Math.floor(y * squareSize) + yc;
   return { x: newx, y: newy };
 }
 
@@ -98,16 +126,69 @@ function createList(begin, end, step, myfunc) {
   }
   return mylist;
 }
-let a = 0;
-let b = 0;
-let c = 0;
+let zeta = 0;
+let wn = 4;
+let gain = 5;
 drawGrid();
+
+
+function stepResponse(t){
+  
+
+  if(zeta >1 || zeta <-1 ){
+    
+    let p1 = -zeta*wn+wn*Math.sqrt(zeta*zeta-1); //poles 
+    let p2 = -zeta*wn-wn*Math.sqrt(zeta*zeta-1);
+    
+    // Y(s) = A/s + B/(s-p1) + C/(s-p2)
+
+
+    let A = 1;
+    let B = (wn*wn)/(p1*(p1-p2));
+    let C = (wn*wn)/(p2*(p2-p1));
+
+    return A+B*Math.exp(p1*t)+C*Math.exp(p2*t);
+
+
+
+  }else if(zeta ===1){
+
+    let p = -wn;
+
+    let A = 1;
+    let B = -1;
+    let C = -wn;
+    return A+B*Math.exp(p*t)+C*t*Math.exp(p*t);
+
+  }else if(zeta <1 && zeta >-1){
+    
+    let alpha = zeta*wn;
+    let wd=wn*Math.sqrt(1-zeta*zeta);
+    
+    return 1-Math.exp(-alpha*t)*Math.cos(wd*t)-(alpha/wd)*Math.exp(-alpha*t)*Math.sin(wd*t);
+
+
+
+  }else if(zeta===-1){
+
+    let p = wn;
+
+    let A = 1;
+    let B = -1;
+    let C = wn;
+    return A+B*Math.exp(p*t)+C*t*Math.exp(p*t);
+    
+  }
+
+
+}
+
 plot(
-  createList(-50, 50, 0.001, (x) => {
+  createList(0, 25, 0.001, (x) => {
     return x;
   }),
-  createList(-50, 50, 0.001, (x) => {
-    return a * Math.sin(x + b) + c;
+  createList(0, 25, 0.001, (x) => {
+    return gain*stepResponse(x);
   }),
   "#f0f8ff",
   1
@@ -116,43 +197,46 @@ plot(
 slider1.oninput = function () {
   ctx.clearRect(0, 0, width, height);
   drawGrid();
-  a = this.value / 7;
+  zeta = this.value / 24;
   plot(
-    createList(-50, 50, 0.001, (x) => {
+    createList(0, 25, 0.001, (x) => {
       return x;
     }),
-    createList(-50, 50, 0.001, (x) => {
-      return a * Math.sin(x + b) + c;
+    createList(0, 25, 0.001, (x) => {
+      return gain*stepResponse(x);
     }),
     "#f0f8ff",
     1
   );
 };
+
 slider2.oninput = function () {
+
   ctx.clearRect(0, 0, width, height);
   drawGrid();
-  b = (this.value / 200) * 2 * Math.PI;
+  wn = this.value / 24;
   plot(
-    createList(-50, 50, 0.001, (x) => {
+    createList(0, 25, 0.001, (x) => {
       return x;
     }),
-    createList(-50, 50, 0.001, (x) => {
-      return a * Math.sin(x + b) + c;
+    createList(0, 25, 0.001, (x) => {
+      return gain*stepResponse(x);
     }),
     "#f0f8ff",
     1
   );
 };
 slider3.oninput = function () {
+
   ctx.clearRect(0, 0, width, height);
   drawGrid();
-  c = this.value / 10;
+  gain = this.value / 24;
   plot(
-    createList(-50, 50, 0.001, (x) => {
+    createList(0, 25, 0.001, (x) => {
       return x;
     }),
-    createList(-50, 50, 0.001, (x) => {
-      return a * Math.sin(x + b) + c;
+    createList(0, 25, 0.001, (x) => {
+      return gain*stepResponse(x);
     }),
     "#f0f8ff",
     1
